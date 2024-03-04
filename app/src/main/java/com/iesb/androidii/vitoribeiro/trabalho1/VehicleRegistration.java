29b73cd5e879d7f0bdb5845e2eba98d5d699a2bf
Vitor Ribeiro <vitor.ribeiro.var@gmail.com>
2024-03-04 17:13:29 -0300

    fist commit

---
+++ app/src/main/java/com/iesb/androidii/vitoribeiro/trabalho1/VehicleRegistration.java
@@ -0,0 +1,14 @@
+package com.iesb.androidii.vitoribeiro.trabalho1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class VehicleRegistration extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vehicle_registration);
+    }
+}
